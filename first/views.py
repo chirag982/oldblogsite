aaf64321df94350543788cd261a97168a9042b31
chirag982 <chirag178g@gmail.com>
2024-10-14 22:27:14 +0530

    Updated signup added user creation feature

--- first/views.py
+++ first/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -8,4 +9,19 @@ def login(request):
     return render(request, 'beforelogin/login.html')
 
 def signup(request):
-    return render(request, 'beforelogin/signup.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        if (User.objects.filter(username=username).exists()):
+            return render(request, 'beforelogin/signup.html', {
+                        "message": "User already exists with this username"
+                    })
+        else:
+            user = User.objects.create_user(username, email, password)
+            return render(request, "beforelogin/login.html", {
+                "message": "User created Successfully.",
+                "username": user.username
+            })    
+    else:
+        return render(request, 'beforelogin/signup.html')
